347c462c94ca49daa0749d4c411931131300b656
harmohanjohal <johalharmohan2121@gmail.com>
2025-12-09 18:43:05 +0000

    changes to output.tf for null public ips for prod

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -14,11 +14,11 @@ output "worker_vm_public_ip" {
 }
 
 output "web_ssh_command" {
-  description = "SSH command for the prod web VM"
-  value       = "ssh ${var.admin_username}@${module.compute_web.vm_public_ip}"
+  description = "SSH command for the prod web VM (if it has a public IP)"
+  value       = module.compute_web.vm_public_ip != null ? "ssh ${var.admin_username}@${module.compute_web.vm_public_ip}" : null
 }
 
 output "worker_ssh_command" {
-  description = "SSH command for the prod worker VM"
-  value       = "ssh ${var.admin_username}@${module.compute_worker.vm_public_ip}"
+  description = "SSH command for the prod worker VM (if it has a public IP)"
+  value       = module.compute_worker.vm_public_ip != null ? "ssh ${var.admin_username}@${module.compute_worker.vm_public_ip}" : null
 }
